71f5e7ca5c1322f98c6a8d4d3837927de91d3891
scoops82 <rupert.taylor1982@gmail.com>
2013-12-10 10:21:54 +0000

    Functional test now tests for numbered list

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-#from django.http import HttpResponse 
-
-# Create your views here.
-#def home_page(request):
-#	return HttpResponse('<html><title>To-Do lists</title></html>')
 def home_page(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', {
+		'new_item_text': request.POST.get('item_text', ''),
+	})
